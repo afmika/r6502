49b4b0e4e99e3d8259c1ab0b69e262f1193fbc7b
Michaël <afmichael73@gmail.com>
2023-05-30 22:19:17 +0300

    prepare compiler

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,10 +1,47 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, rc::Rc, cmp::min};
+use crate::asm_lexer::Token;
 
-pub struct Compiler { }
+pub struct Compiler {
+    tokens: Rc<Vec<Token>>,
+    cursor: usize
+}
 
 impl Compiler {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(tokens: Rc<Vec<Token>>) -> Self {
+        Self {
+            tokens,
+            cursor: 0
+        }
+    }
+
+    pub fn toByteCode(&mut self) -> Vec<u8> {
+        loop {
+            break;
+        }
+        vec![]
+    }
+
+    fn is_eof(&mut self) -> bool {
+        *self.curr() == Token::EOF
+    }
+
+    fn curr(&mut self) -> &Token {
+        self
+            .tokens
+            .get(self.cursor)
+            .unwrap_or(&Token::EOF)
+    }
+
+    fn next(&mut self) -> &Token {
+        self.cursor = min(self.tokens.len(), self.cursor + 1);
+        return self.curr();
+    }
+
+    fn prev(&mut self) -> &Token {
+        if self.cursor > 0 {
+            self.cursor -= 1;
+        }
+        return self.curr();
     }
 
     pub fn write(path: PathBuf) {
